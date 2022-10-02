4a5c915b68416725a9c50e2d0a0b3f6970e8b856
George Krachtopoulos <georgekraxt@gmail.com>
2022-10-02 19:20:48 +0300

    Fix CSS shown stylesheets | Commit 3

--- site/modules/layout/components/Navbar/Navbar.tsx
+++ site/modules/layout/components/Navbar/Navbar.tsx
@@ -27,8 +27,8 @@ const Navbar = ({ links = [], root, ...props }: Props): ComponentElement => (
       px="container.px"
       {...props}
     >
-      <Flex align="inherit" gap={6}>
-        <Link
+      {/* <Flex align="inherit" gap={6}> */}
+        {/* <Link
           href="/"
           aria-label="Logo"
           borderRadius="full"
@@ -38,7 +38,7 @@ const Navbar = ({ links = [], root, ...props }: Props): ComponentElement => (
           _hover={{ boxShadow: "md", transform: "scale(1.05)" }}
         >
           <Logo />
-        </Link>
+        </Link> */}
         {/* <Flex as="nav" gap={4} display={{ base: "none", md: "flex" }}>
           {[{ label: "All products", href: "/search" }].concat(links).map(({ label, href }) => (
             <Link key={href} href={href} color="gray.800">
@@ -46,8 +46,8 @@ const Navbar = ({ links = [], root, ...props }: Props): ComponentElement => (
             </Link>
           ))}
         </Flex> */}
-      </Flex>
-      <UserNav />
+      {/* </Flex> */}
+      {/* <UserNav /> */}
     </Flex>
   </NavbarRoot>
 );
